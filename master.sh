cb23b2241ea5b40f4b0455c367e0b43edda46439
ladworkin <lad@sund.ku.dk>
2022-04-12 00:49:15 +0200

    tracking version numbers for required R and python packages

--- master.sh
+++ master.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-# docker run --rm -it -v "$PWD":"$PWD" -w "$PWD" "$R_IMAGE" "$database_id" "$tissue_id".h5ad.zip "$tissue" .
+DOCKER_IMAGE=${DOCKER_IMAGE:-"figure_generation_pipeline:latest"}
 
-# R_IMAGE=${R_IMAGE:-"panglao_pipeline:latest"}
+docker run --rm -it -v "$PWD/":"$PWD" -w "$PWD" "$DOCKER_IMAGE"
+
